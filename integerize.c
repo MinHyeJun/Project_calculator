4d4efe70c2a885c8cac20631b97520687a840f32
MinHyeJun <m2j9702@naver.com>
2016-05-29 00:55:21 +0900

    Revert "change integerize.c"
    This reverts commit 98cd01c81f24ef5a7be62082ccacf6e296eb53ec.

--- integerize.c
+++ integerize.c
@@ -1,9 +1,7 @@
-void integerize(char num[]) //실수 정수화 완료.
+void integerize(char num[]) //실수 정수화 완료. ex)1.3 >> 1300000000, 0.00002 >> 20000
 {
-	int i,point=0,cnt=0,cnt_0=0,cnt_f=0,cnt_str=0;
-	//i는 for문 임시변수, point 소수점의 위치,cnt 0을 붙인 후의 배열의 길이
-	//cnt_0 앞의 0을 제거하기 위한 0의 개수
-	//cnt_f 소수점 이후부터의 배열길이, cnt_str 처음 입력 받은 배열의 길이
+	int i,point,cnt_0=0,cnt=0;
+	//i는 for문 위한 임시 변수, point는 소수점의 위치, cnt_0은 앞의 0을 제거하기위한 0의 개수,cnt는 배열의 길이
 
 	for(i=0;num[i]!='\0';i++)
 	{
@@ -13,23 +11,21 @@ void integerize(char num[]) //실수 정수화 완료.
 
 			for(i=point;num[i]!='\0';i++)
 			{	
-				cnt_f++;
+				cnt++;
 				num[i]=num[i+1];
 			}
+
+			break;
 		}
-		cnt_str++;
 	}
-	if(point>0)
-	{
-		for(i=point-1+cnt_f;i<=point-1+9;i++)
-			num[i]='0';
-	}else{
-		 for(i=cnt_str;i<=cnt_str+8;i++)
-			  num[i]='0';
-	}
+
+	for(i=point-1+cnt;i<=point-1+9;i++)
+		num[i]='0';
 
 	for(i=0;num[i]=='0';i++)
 		cnt_0++;
+	
+	cnt=0;
 
 	for(i=0;num[i]!='\0';i++)
 		cnt++;
